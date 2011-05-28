49af965b6b8155a176d9940117a05cbcce36f8a1
Alaric Holloway <emumonger@yahoo.com>
2011-05-28 15:06:02 -0700

    New GuiButtonFamily class

---
+++ web/htdocs/js/alberti/gui/GuiButtonFamily.js
@@ -0,0 +1,43 @@
+/*
+ * GuiButtonFamily.js
+ * 
+ * A family of related GuiButtons. Only one button in each GuiButtonFamily can
+ * be toggled to its 'on' state at a time. GuiButtonFamily handles this
+ * toggling automatically via its toggleButton method.
+ * 
+ * * */
+ 
+function GuiButtonFamily() {
+	this.buttons = [];
+	this.currentToggled = -1;         // The currently toggled button's index
+}
+
+// Expects a previously-added GuiButton object. Toggles it 'on'.
+GuiButtonFamily.prototype.toggleButton = function(button) {
+	var index = this.buttons.indexOf(button);
+	
+	if (index < 0) {
+		throw "Invalid GuiButton object passed to GuiButtonFamily::toggleButton.";
+	}
+	
+	if (this.currentToggled >= 0) {
+		this.buttons[this.currentToggled].toggle(false);
+	}
+	
+	this.buttons[index].toggle(true);
+	this.currentToggled = index;
+};
+
+GuiButtonFamily.prototype.addButton = function(button) {
+	if (this.buttons.indexOf(button) < 0) {
+		this.buttons.push(button);
+	}
+};
+
+GuiButtonFamily.prototype.removeButton = function(button) {
+	var index = this.buttons.indexOf(button);
+	
+	if (index >= 0) {
+		this.buttons.splice(index, 1);
+	}
+};
