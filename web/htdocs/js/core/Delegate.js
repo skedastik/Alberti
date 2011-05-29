d057dfde76b14cd9432b27d52a9918e319d135a8
Alaric Holloway <emumonger@yahoo.com>
2011-05-28 20:41:46 -0700

    Delegate auto-generates getters/setters

--- web/htdocs/js/core/Delegate.js
+++ web/htdocs/js/core/Delegate.js
@@ -2,8 +2,10 @@
  * Delegate.js
  * 
  * Delegate allows you to take an object instance and generate a delegate
- * object with additional functionality. This is useful for connecting a data 
- * object with its corresponding view (e.g., every time an Array's "push" 
+ * object with additional functionality. As far as other objects are 
+ * concerned, this new delegate is identical to the original object (excepting
+ * use of the instanceof operator, of course). This is useful for connecting a 
+ * data object with its corresponding view (e.g., every time an Array's "push" 
  * method is invoked, add the pushed element to a corresponding HTML table).
  * 
  * USAGE
@@ -12,66 +14,122 @@
  * Delegate. In the subclass' constructor, call Delegate::mapMethod once for
  * each method that needs delegation, e.g.:
  * 
- *    // Need to delegate 'push' method of Array 'data'
- *    var data = [];
+ *    // Need to delegate 'push' method of Stack object 'data'
+ *    var data = new Stack();
  *    
  *    // So create an appropriate Delegate subclass
- *    function ArrayDelegate(object, ...) {
+ *    function StackDelegate(stack) {
  *       // Call Delegate constructor using your own inheritance scheme
- *       callBaseClassConstructor(object);
- *       // Map 'push' to ArrayDelegate's 'pushDelegate' method
+ *       callBaseClassConstructor(stack);
+ *       // Map 'push' to StackDelegate's 'pushDelegate' method
  *       this.mapMethod("push", "pushDelegate");
  *    }
  *    
- *    // Define "pushDelegate" with identical args to Array::push
- *    ArrayDelegate.prototype.pushDelegate = function(element) {
+ *    // Define "pushDelegate" with identical args to Stack::push
+ *    StackDelegate.prototype.pushDelegate = function(element) {
  *       // Do something with data
  *       addElementToHtmlTable(element);
  *    }
  *    
- *    // Create an instance of ArrayDelegate tied to Array 'data'
- *    var myData = new ArrayDelegate(data);
+ *    // Create an instance of StackDelegate tied to Stack 'data'
+ *    var myData = new StackDelegate(data);
  *    
  *    // Push into 'data'. String "foo" automatically appears in HTML table.
  *    myData.push("foo");
  * 
  * Notice that the pushDelegate method above did not have to invoke the 'push'
- * method of Array 'data'. This is automatically performed by the Delegate
+ * method of Stack 'data'. This is automatically performed by the Delegate
  * class, _before_ invoking pushDelegate.
  * 
  * You can disable delegation by calling Delegate::disableDelegation.
  * Subsequent calls to object methods will invoke the object method directly,
- * without invoking the delegate. To re-enable, call 'enableDelegation'.
+ * without invoking the delegate method. To re-enable, use 'enableDelegation'.
+ * 
+ * Delegate automatically generates getters and setters for the properties of
+ * the delegated object, so you can access them directly. Taking the above
+ * example, and assuming the Stack object has a property called 'length':
+ * 
+ *    myData.length == data.length;   // evaluates to true
+ * 
+ * Keep in mind that if a property is subsequently added to the delegated 
+ * object, it will _not_ be added to the corresponding Delegate object:
+ * 
+ *    data.foo = "bar";
+ *    myData.foo;                     // undefined
+ * 
+ * One limitation of Delegate is that you cannot create a delegate for an 
+ * instance of an Array object. Attempting to do so will generate an 
+ * exception.
+ * 
+ * Also, the delegated object may share a property name with the Delegate. An 
+ * exception will be thrown if this is the case (though it is unlikely).
  * 
  * * */
  
-function Delegate(object) {
-	this.object = object;
-	this.enabled = true;
+function Delegate(object, methodMap) {
+	this.delegatedObject = object;
+	this.delegationEnabled = true;
+	
+	if (Array.isArray(object)) {
+		throw "Delegate constructor > Creating delegates for Array objects is not supported.";
+	}
+	
+	var props = [];        // Collects non-function properties
+	var funcs = [];        // Collects function properties
+	
+	for (var prop in this.delegatedObject) {
+		if (this[prop] !== undefined) {
+			// Assert that there is no intersection of delegated object property names and internal property names.
+			throw "Delegate constructor > Delegated object has property ('"+prop+"') with same name as internal property.";
+		}
+		
+		if (typeof this.delegatedObject[prop] == "function") {
+			funcs.push(prop);
+		} else {
+			props.push(prop);
+		}
+	}
+	
+	// Generate getters and setters for each non-function property
+	props.forEach(function(propName) {
+		if (Object.defineProperty) {
+			// ECMAScript 5
+			Object.defineProperty(this, propName, {
+				get: function()      { return this.delegatedObject[propName]; },
+				set: function(value) { this.delegatedObject[propName] = value; }
+			});
+		} else {
+			// Legacy
+			this.__defineGetter__(propName, function()      { return this.delegatedObject[propName]; });
+			this.__defineSetter__(propName, function(value) { this.delegatedObject[propName] = value; });
+		}
+	}, this);
+	
+	// Generate internal methods that simply apply methods of delegated object
+	funcs.forEach(function(funcName) {
+		this[funcName] = function() {
+			this.delegatedObject[funcName].apply(this.delegatedObject, arguments);
+		};
+	}, this);
 }
 
 Delegate.prototype.enableDelegation = function() {
-	this.enabled = true;
+	this.delegationEnabled = true;
 };
 
 Delegate.prototype.disableDelegation = function() {
-	this.enabled = false;
+	this.delegationEnabled = false;
 };
 
 // Map object method to internal method
 Delegate.prototype.mapMethod = function(objectMethodName, internalMethodName) {
-	// Assert that mapped method does not override existing method
-	if (this[objectMethodName] !== undefined) {
-		throw "Delegate::mapMethod > Method name '"+objectMethodName+"' would override existing method.";
-	}
-	
-	// Generate member method with same name as delegated object's method
+	// Override internal method
 	this[objectMethodName] = function() {
 		// Invoke delegated object's method
-		this.object[objectMethodName].apply(this.object, arguments);
+		this.delegatedObject[objectMethodName].apply(this.delegatedObject, arguments);
 		
-		// If delegation enabled, invoke internal method delegate
-		if (this.enabled) {
+		// If delegation is enabled, invoke internal delegate method
+		if (this.delegationEnabled) {
 			this[internalMethodName].apply(this, arguments);
 		}
 	};
