205124c8f7351d55486a12540cf585e38a2b0f08
Alaric Holloway <emumonger@yahoo.com>
2011-05-29 20:31:31 -0700

    Layer switching via LayerManagerDelegate

--- web/htdocs/js/alberti/gui/LayerManagerDelegate.js
+++ web/htdocs/js/alberti/gui/LayerManagerDelegate.js
@@ -8,8 +8,10 @@
 function LayerManagerDelegate(layerManager, layerPanel) {
 	LayerManagerDelegate.baseConstructor.call(this, layerManager);
 	this.layerPanel = layerPanel;
+	
 	this.mapMethod("insertLayer", "insertLayerDelegate");
 	this.mapMethod("deleteCurrentLayer", "deleteCurrentLayerDelegate");
+	this.mapMethod("switchToLayer", "switchToLayerDelegate");
 }
 Util.extend(LayerManagerDelegate, Delegate);
 
@@ -19,4 +21,9 @@ LayerManagerDelegate.prototype.insertLayerDelegate = function(layer, before) {
 
 LayerManagerDelegate.prototype.deleteCurrentLayerDelegate = function() {
 	Dbug.log("LayerManagerDelegate::deleteCurrentLayerDelegate");
+};
+
+LayerManagerDelegate.prototype.switchToLayerDelegate = function(layerNumber) {
+	Dbug.log("LayerManagerDelegate::switchToLayerDelegate");
+	this.layerPanel.selectRow(layerNumber);
 };
