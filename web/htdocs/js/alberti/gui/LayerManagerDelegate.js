f9e1d6a2bcb686902e1328ce44ad988261f93ca2
Alaric Holloway <emumonger@yahoo.com>
2011-06-02 03:27:43 -0700

    Layer deletion handled entirely in post-delegate

--- web/htdocs/js/alberti/gui/LayerManagerDelegate.js
+++ web/htdocs/js/alberti/gui/LayerManagerDelegate.js
@@ -10,7 +10,7 @@ function LayerManagerDelegate(layerManager, lpController) {
 	this.lpController = lpController;
 	
 	this.mapMethod("insertLayer", "insertLayerPreDelegate", "insertLayerPostDelegate");
-	this.mapMethod("deleteLayer", "deleteLayerPreDelegate", "deleteLayerPostDelegate");
+	this.mapMethod("deleteLayer", null, "deleteLayerDelegate");
 	this.mapMethod("switchToLayer", "switchToLayerDelegate");
 	this.mapMethod("setLayerVisibility", null, "setLayerVisibilityDelegate");
 	this.mapMethod("setLayerName", "setLayerNameDelegate");
@@ -20,11 +20,7 @@ Util.extend(LayerManagerDelegate, Delegate);
 LayerManagerDelegate.prototype.insertLayerPreDelegate = function(newLayer, refLayer, before) {
 	refLayer = refLayer ? refLayer : this.delegatedObject.currentLayer;
 	
-	Dbug.log("about to insert");
-	
 	if (before) {
-		Dbug.log("refLayer:");
-		Dbug.log(refLayer);
 		// Layer is being inserted before reference layer, so use reference layer as 'beforeRow' arg
 		this.lpController.insertNewRow(newLayer, refLayer);
 	} else {
@@ -36,19 +32,14 @@ LayerManagerDelegate.prototype.insertLayerPreDelegate = function(newLayer, refLa
 			this.lpController.insertNewRow(newLayer, this.layers[this.layers.indexOf(refLayer) + 1]);
 		}
 	}
-	
-	Dbug.log("about to insert");
 };
 
 LayerManagerDelegate.prototype.insertLayerPostDelegate = function(newLayer, refLayer, before) {
 	// TODO: Update visibility toggle after layer insertions
 };
 
-LayerManagerDelegate.prototype.deleteLayerPreDelegate = function(targetLayer, invertSwitch) {
+LayerManagerDelegate.prototype.deleteLayerDelegate = function(targetLayer, invertSwitch) {
 	this.lpController.deleteRow(targetLayer);
-};
-
-LayerManagerDelegate.prototype.deleteLayerPostDelegate = function(targetLayer, invertSwitch) {
 	// TODO: Update visibility toggle after layer deletions
 };
 
