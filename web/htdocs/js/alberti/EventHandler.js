1e89831fb970342e1eff321d1bc3423929d97583
Alaric Holloway <emumonger@yahoo.com>
2011-06-04 22:27:37 -0700

    Discard bubbling mouseover and mouseout events

--- web/htdocs/js/alberti/EventHandler.js
+++ web/htdocs/js/alberti/EventHandler.js
@@ -10,12 +10,17 @@ function EventHandler() {
 }
 
 EventHandler.prototype.handleEvent = function(evt) {
-	// Put a limit on how many mousemove events are processed per second
 	if (evt.type == "mousemove") {
+		// Put a limit on how many mousemove events are processed per second
 		if (Date.now() - this.lastMouseMove < Alberti.refreshms) {
 			return;
 		} else {
 			this.lastMouseMove = Date.now();
+		}
+	} else if (evt.type == "mouseover" || evt.type == "mouseout") {
+		// Discard bubbling mouseover and mouseout events
+		if (evt.target != evt.currentTarget || Util.hasChild(evt.currentTarget, evt.relatedTarget)) {
+			return;
 		}
 	}
 	
