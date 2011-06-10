6d3cacb47f3271a5b150eac01007d126a1e22070
Alaric Holloway <emumonger@yahoo.com>
2011-06-10 14:59:04 -0700

    New class ClipBoard for copy/paste functionality

---
+++ web/htdocs/js/alberti/ClipBoard.js
@@ -0,0 +1,28 @@
+/*
+ * ClipBoard.js
+ * 
+ * Copy/paste facilities for Alberti.
+ * 
+ * * */
+ 
+function ClipBoard() {
+	this.shapes = [];
+}
+
+// Copy Shape objects in given array to clip board
+ClipBoard.prototype.copy = function(shapeArray) {
+	if (shapeArray.length > 0) {
+		this.shapes = [];
+	
+		for (var i = 0, len = shapeArray.length; i < len; i++) {
+			this.shapes.push(shapeArray[i].clone());
+		}
+	}
+};
+
+// Paste shapes from clip board into current layer of given LayerManager
+ClipBoard.prototype.paste = function(layerManager) {
+	for (var i = 0, len = this.shapes.length; i < len; i++) {
+		layerManager.insertShape(this.shapes[i].clone().generate());
+	}
+};
