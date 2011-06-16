83193a654cc571745d72b91e06d9d84f60f57645
Alaric Holloway <emumonger@yahoo.com>
2011-06-15 23:00:29 -0700

    Configuration globals for Alberti app

---
+++ web/htdocs/js/alberti/Conf.js
@@ -0,0 +1,33 @@
+Alberti.fpsMax    = Animation.fpsMax;
+Alberti.refreshms = Math.round(1000 / Alberti.fpsMax);      // limit redraw rate
+
+Alberti.showToolTips = true;                            // Show tool helper tips?
+
+Alberti.maxUndos = 100;                                  // Maximum number of undo actions
+
+// All numerical SVG attributes are rounded to this decimal place so as to
+// leave an adequate number of significant digits for large numerical values. 
+// This value is also used when checking equality between two floats (see 
+// Util.equals method).
+Alberti.decimalPrecision = 3;
+
+// useful constants for various calculations
+Alberti.halfOriginalWindowWidth = Math.round(window.innerWidth / 2);
+Alberti.halfOriginalWindowHeight = Math.round(window.innerHeight / 2);
+
+Alberti.optimizedClassName = "optimized";                // special CSS class used to render non-anti-aliased line
+Alberti.crispLines = true;                               // if true, lines are always rendered with the above CSS style
+
+// For whatever reason, SVG rendering speed slows down drastically for line
+// widths less than or equal to 1. Hence, line width must be increased 
+// slightly during optimized rendering.
+Alberti.defaultLineWidth = 1;
+Alberti.lineWidthDeltaOptimized = 0.01;
+
+// Set to true to enable non-scaling lines. This is only useful if the SVG 
+// implementation does not support "vector-effect: non-scaling-stroke".
+Alberti.nonScalingLinesHack = true;
+
+// Set to true to enable server-side save script. This automatically forces a
+// save dialog when saving Alberti documents.
+Alberti.usePhpSaveScript = false;
