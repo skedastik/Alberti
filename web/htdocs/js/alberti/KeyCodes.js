caca0175ba41d07f81dfc54cfeaee5f564b2534d
Alaric Holloway <emumonger@yahoo.com>
2011-06-16 21:00:51 -0700

    Move keycode globals into separate file

---
+++ web/htdocs/js/alberti/KeyCodes.js
@@ -0,0 +1,37 @@
+/*
+ * KeyCodes.js
+ * 
+ * * */
+
+var KeyCode = {
+	shift: 16,
+	ctrl:  17,
+	alt:   18,
+	esc:   27,
+    
+	del:       46,
+	backspace: 8,
+	enter:     13,
+    
+	arrowUp:   38,
+	arrowDown: 40,
+    
+	number1: 49,
+	number2: 50,
+	number3: 51,
+	number4: 52,
+	number5: 53,
+	number6: 54,
+	number7: 55,
+	number8: 56,
+	number9: 57,
+    
+	lpCollapse: 220,       // '\' - Collapse/reveal layer panel
+	snap:       70,        // 'f' - Activate snap-to-intersection
+	undo:       85,        // 'u'
+	redo:       82,        // 'r'
+	cut:        88,        // 'x'
+	paste:      86,        // 'v'
+	save:       83,        // 's' - Save the document
+	load:       79         // 'o' - Open a document
+};
