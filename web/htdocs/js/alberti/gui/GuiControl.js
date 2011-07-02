6ba59022921507e5dc2d85bc3d673efc380364f9
Alaric Holloway <emumonger@yahoo.com>
2011-07-01 18:04:25 -0700

    GuiControl constructor expects default action. New 'connect' method.

--- web/htdocs/js/alberti/gui/GuiControl.js
+++ web/htdocs/js/alberti/gui/GuiControl.js
@@ -6,17 +6,19 @@
  * 
  * USAGE
  * 
- * The constructor expects three arguments. 'id' is an ID string. 'elt' is an 
- * HTML element representing the control. The 'delegate' argument is a 
- * reference to a controller object.
+ * The constructor expects four arguments: 'id' is an ID string. 'elt' is an 
+ * HTML element representing the control. 'controller' is a reference to a 
+ * controller object. 'action' is a method belonging to the controller and
+ * defines the default action of the GuiControl.
  * 
  * * */
  
-function GuiControl(id, elt, delegate) {
+function GuiControl(id, elt, controller, action) {
 	GuiControl.baseConstructor.call(this);
 	this.id = id;
 	this.htmlNode = elt;
-	this.delegate = delegate;
+	this.controller = controller;
+	this.action = action;
 }
 Util.extend(GuiControl, EventHandler);
 
@@ -24,11 +26,19 @@ GuiControl.prototype.getId = function() {
 	return this.id;
 };
 
+// Connect a GuiControl to a controller object and default action. Returns self.
+GuiControl.prototype.connect = function(controller, action) {
+	this.controller = controller;
+	this.action = action;
+	
+	return this;
+};
+
 // Invoke the given action with following arguments. Returns result of action method.
 GuiControl.prototype.invokeAction = function(action) {
-	if (this.delegate[action]) {
+	if (this.controller[action]) {
 		var args = Array.prototype.slice.call(arguments, 1);
-		return this.delegate[action].apply(this.delegate, args);
+		return this.controller[action].apply(this.controller, args);
 	}
 };
 
