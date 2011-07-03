2c26c864096168825d211011ce04dde3d618fbbc
Alaric Holloway <emumonger@yahoo.com>
2011-07-02 19:49:54 -0700

    Corrected typo in documentation

--- web/htdocs/js/alberti/Tool.js
+++ web/htdocs/js/alberti/Tool.js
@@ -88,7 +88,7 @@
  * You may display tool tips by calling Tool::displayTip, which expects the
  * same arguments as ToolTip::setText.
  * 
- * complete(stepNum, constrain)
+ * complete(stepNum)
  * 
  * This method is called upon completion of the tool, either by reaching the
  * final step, or by the user short-circuiting the tool with the enter key.
