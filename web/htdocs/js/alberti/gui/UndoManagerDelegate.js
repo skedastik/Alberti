b3b793fc7aa1c26d2d72b1e8f67326e3fadfdf59
Alaric Holloway <emumonger@yahoo.com>
2011-07-02 23:43:52 -0700

    Map recordStop instead of recordStart. Propagate changes to UI.

--- web/htdocs/js/alberti/gui/UndoManagerDelegate.js
+++ web/htdocs/js/alberti/gui/UndoManagerDelegate.js
@@ -9,8 +9,8 @@ function UndoManagerDelegate(undoManager, ui) {
 	UndoManagerDelegate.baseConstructor.call(this, undoManager);
 	this.ui = ui;
 	
-	this.mapMethod("push",        "pushDelegate");
-	this.mapMethod("recordStart", "pushDelegate");
+	this.mapMethod("push", null, "pushDelegate");
+	this.mapMethod("recordStop", null, "pushDelegate");
 	
 	this.mapMethod("undo", null, "undoDelegate");
 	this.mapMethod("redo", null, "undoDelegate");
@@ -19,7 +19,7 @@ Util.extend(UndoManagerDelegate, Delegate);
 
 UndoManagerDelegate.prototype.pushDelegate = function() {
 	// For efficiency's sake, do not invoke the undo delegate for every push 
-	// recorded to a buffered action. Only the first call to recordStart is 
+	// recorded to a buffered action. Only the last call to recordStop is 
 	// significant (and only then will bufferLevel == 0).
 	if (this.enabled && this.bufferLevel == 0) {
 		this.undoDelegate();
@@ -27,5 +27,5 @@ UndoManagerDelegate.prototype.pushDelegate = function() {
 };
 
 UndoManagerDelegate.prototype.undoDelegate = function() {
-	Dbug.log("Stack changed.");
+	this.ui.updateUndoMenuItems(this.undoStack.length > 0, this.redoStack.length > 0);
 };
