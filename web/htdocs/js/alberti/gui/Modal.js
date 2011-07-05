f14d5e498fecf23557d8f145d1fa60fbf8b35f5e
Alaric Holloway <emumonger@yahoo.com>
2011-07-04 20:05:06 -0700

    Hide modal when user presses esc. Added GPL header.

--- web/htdocs/js/alberti/gui/Modal.js
+++ web/htdocs/js/alberti/gui/Modal.js
@@ -1,4 +1,23 @@
-/*
+/*  
+ *  Copyright (C) 2011, Alaric Holloway <alaric.holloway@gmail.com>
+ *  
+ *  This file is part of Alberti.
+ *
+ *  Alberti is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  Alberti is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with Alberti.  If not, see <http://www.gnu.org/licenses/>.
+ *  
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ *
  * Modal.js
  * extends EventHandler
  * 
@@ -38,10 +57,12 @@ Modal.prototype.show = function() {
 	this.popupDiv.style.display = "";
 	
 	this.registerListener("mousedown", this.closeBoxDiv, false);
+	this.registerListener("keydown", window, false);
 };
 
 Modal.prototype.hide = function() {
 	this.unregisterListener("mousedown", this.closeBoxDiv, false);
+	this.unregisterListener("keydown", window, false);
 	
 	this.popupDiv.style.display = "none";
 	this.curtainDiv.parentNode.removeChild(this.curtainDiv);
@@ -50,4 +71,10 @@ Modal.prototype.hide = function() {
 
 Modal.prototype.mousedown = function(evt) {
 	this.hide();
+};
+
+Modal.prototype.keydown = function(evt) {
+	if (evt.keyCode == KeyCode.esc) {
+		this.hide();
+	}
 };
