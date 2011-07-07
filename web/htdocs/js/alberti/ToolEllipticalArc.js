a0c1fdf13e2934cf60a0d9533fbc90452a371b67
Alaric Holloway <emumonger@yahoo.com>
2011-07-06 20:50:14 -0700

    More functionality for elliptical arc tool

--- web/htdocs/js/alberti/ToolEllipticalArc.js
+++ web/htdocs/js/alberti/ToolEllipticalArc.js
@@ -21,12 +21,14 @@
  * ToolEllipticalArc.js
  * extends ToolArc
  * 
- * Tool for drawing elliptical arcs inscribed within quadrilaterals.
+ * Tool for drawing elliptical arcs inscribed within convex quadrilaterals.
  * 
  * * */
  
 function ToolEllipticalArc(uiObjects) {
-	ToolEllipticalArc.baseConstructor.call(this, -1, 6, false, uiObjects);
+	ToolEllipticalArc.baseConstructor.call(this, -1, 4, false, uiObjects);
+	
+	this.quadPoints = [];            // Contains points of quadrilateral
 }
 Util.extend(ToolEllipticalArc, ToolArc);
 
@@ -37,11 +39,37 @@ ToolEllipticalArc.prototype.executeStep = function(stepNum, gx, gy) {
 		case 1:
 		case 2:
 			var p = Point.fromCoord(new Coord2D(gx, gy)).generate();
+			
+			this.quadPoints[stepNum] = p.coord;
 			this.registerShape(p, "quad_point"+stepNum);
 			break;
 		
 		case 3:
+			var mouseCoord = new Coord2D(gx, gy);
+			var hull = Coord2D.convexHull([this.quadPoints[0], this.quadPoints[1], this.quadPoints[2], mouseCoord]);
 			
+			if (hull.length == 4) {
+				var p = Point.fromCoord(mouseCoord).generate();
+				var ellipse = Ellipse.projectedToQuad(hull[0], hull[1], hull[2], hull[3]).generate();
+				var l1 = Line.fromPoints(hull[0], hull[1]).generate();
+				var l2 = Line.fromPoints(hull[1], hull[2]).generate();
+				var l3 = Line.fromPoints(hull[2], hull[3]).generate();
+				var l4 = Line.fromPoints(hull[3], hull[0]).generate();
+				
+				this.quadPoints[stepNum] = p.coord;
+				this.registerShape(p, "quad_point"+stepNum);
+				this.registerShape(l1, "quad_line1", true);
+				this.registerShape(l2, "quad_line2", true);
+				this.registerShape(l3, "quad_line3", true);
+				this.registerShape(l4, "quad_line4", true);
+				this.registerShape(ellipse, "ellipse_guide", true);
+			} else {
+				// Do not advance to the next step if the convex hull has 
+				// less than four points as this indicates the quadrilateral 
+				// was not, in fact, convex.
+				this.decrementStep();
+			}
+			break;
 	}
 };
 
