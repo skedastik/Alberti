04f735a0a94fd10baa36bfe028a0a67a199f3629
Alaric Holloway <emumonger@yahoo.com>
2011-07-09 12:03:17 -0700

    Move method from Ellipse class to here. Push coefficient attributes.

--- web/htdocs/js/alberti/EllipticalShape.js
+++ web/htdocs/js/alberti/EllipticalShape.js
@@ -60,6 +60,18 @@ EllipticalShape.prototype.push = function() {
 	this.set("berti:rx", this.rx, Alberti.customns);
 	this.set("berti:ry", this.ry, Alberti.customns);
 	this.set("berti:xrot", this.xrot, Alberti.customns);
+	
+	// This will work so long as all elliptical shapes are generated via the
+	// projectedToQuad method. Otherwise coefficients will have to be 
+	// calculated on the fly.
+	if (this.coeffs.length > 0) {
+		this.set("berti:a", this.coeffs[0], Alberti.customns);
+		this.set("berti:b", this.coeffs[1], Alberti.customns);
+		this.set("berti:c", this.coeffs[2], Alberti.customns);
+		this.set("berti:d", this.coeffs[3], Alberti.customns);
+		this.set("berti:f", this.coeffs[4], Alberti.customns);
+		this.set("berti:g", this.coeffs[5], Alberti.customns);
+	}
 };
 
 EllipticalShape.prototype.pull = function() {
@@ -67,6 +79,12 @@ EllipticalShape.prototype.pull = function() {
 	this.rx = this.get("rx", Alberti.customns);
 	this.ry = this.get("ry", Alberti.customns);
 	this.xrot = this.get("xrot", Alberti.customns);
+	
+	var a = this.get("a", Alberti.customns);
+	
+	if (a) {
+		
+	}
 };
 
 // Returns the point on the ellipse at the given polar angle
@@ -88,4 +106,89 @@ EllipticalShape.prototype.getPointGivenAngle = function(a) {
 		this.center.x + this.rx * cos(t) * cos(this.xrot) - this.ry * sin(t) * sin(this.xrot),
 		this.center.y + this.rx * cos(t) * sin(this.xrot) + this.ry * sin(t) * cos(this.xrot)
 	);
+};
+
+// Returns elliptical shape 'e' inscribed in a convex quadrilateral defined by
+// four Coord2D's (which must be passed in anti-clockwise order), as described
+// here:
+//
+//    <http://chrisjones.id.au/Ellipses/ellipse.html>
+//
+// With further equations from:
+// 
+//    http://mathworld.wolfram.com/Ellipse.html
+//
+// Returns 'e'.
+//
+EllipticalShape.projectedToQuad = function(e, w, x, y, z) {
+	var W0 = w.x, W1 = w.y;
+	var X0 = x.x, X1 = x.y;
+	var Y0 = y.x, Y1 = y.y;
+	var Z0 = z.x, Z1 = z.y;
+	
+	var A =  X0*Y0*Z1 - W0*Y0*Z1 - X0*Y1*Z0 + W0*Y1*Z0 - W0*X1*Z0 + W1*X0*Z0 + W0*X1*Y0 - W1*X0*Y0;
+	var B =  W0*Y0*Z1 - W0*X0*Z1 - X0*Y1*Z0 + X1*Y0*Z0 - W1*Y0*Z0 + W1*X0*Z0 + W0*X0*Y1 - W0*X1*Y0;
+	var C =  X0*Y0*Z1 - W0*X0*Z1 - W0*Y1*Z0 - X1*Y0*Z0 + W1*Y0*Z0 + W0*X1*Z0 + W0*X0*Y1 - W1*X0*Y0;
+	var D =  X1*Y0*Z1 - W1*Y0*Z1 - W0*X1*Z1 + W1*X0*Z1 - X1*Y1*Z0 + W1*Y1*Z0 + W0*X1*Y1 - W1*X0*Y1;
+	var E = -X0*Y1*Z1 + W0*Y1*Z1 + X1*Y0*Z1 - W0*X1*Z1 - W1*Y1*Z0 + W1*X1*Z0 + W1*X0*Y1 - W1*X1*Y0;
+	var F =  X0*Y1*Z1 - W0*Y1*Z1 + W1*Y0*Z1 - W1*X0*Z1 - X1*Y1*Z0 + W1*X1*Z0 + W0*X1*Y1 - W1*X1*Y0;
+	var G =  X0*Z1    - W0*Z1    - X1*Z0    + W1*Z0    - X0*Y1    + W0*Y1    + X1*Y0    - W1*Y0;
+	var H =  Y0*Z1    - X0*Z1    - Y1*Z0    + X1*Z0    + W0*Y1    - W1*Y0    - W0*X1    + W1*X0;
+	var I =  Y0*Z1    - W0*Z1    - Y1*Z0    + W1*Z0    + X0*Y1    - X1*Y0    + W0*X1    - W1*X0;
+	
+	var S = Matrix.create([
+		[A,B,C],
+		[D,E,F],
+		[G,H,I]
+	]);
+	
+	var T = S.inverse();
+		
+	var J = T.elements[0][0];
+	var K = T.elements[0][1];
+	var L = T.elements[0][2];
+	var M = T.elements[1][0];
+	var N = T.elements[1][1];
+	var O = T.elements[1][2];
+	var P = T.elements[2][0];
+	var Q = T.elements[2][1];
+	var R = T.elements[2][2];
+	
+	var a = J*J + M*M - P*P;
+	var b = J*K + M*N - P*Q;
+	var c = K*K + N*N - Q*Q;
+	var d = J*L + M*O - P*R;
+	var f = K*L + N*O - Q*R;
+	var g = L*L + O*O - R*R;
+	
+	var bSquaredMinusAC = (b*b - a*c);
+	
+	e.center.x = (c*d - b*f) / bSquaredMinusAC;
+	e.center.y = (a*f - b*d) / bSquaredMinusAC;
+	
+	var numerator = (2 * (a*f*f + c*d*d + g*b*b - 2*b*d*f - a*c*g));
+	var rootQuantity = Math.sqrt((a - c)*(a - c) + 4*b*b);
+	
+	e.rx = Math.sqrt(numerator / (bSquaredMinusAC * (rootQuantity - (a+c))));
+	e.ry = Math.sqrt(numerator / (bSquaredMinusAC * (-rootQuantity - (a+c))));
+	
+	if (b == 0) {
+		if (a < c) {
+			e.xrot = 0;
+		} else {
+			e.xrot = halfPi;
+		}
+	} else {
+		var cotanQuantity = 0.5 * atan((2*b) / (a - c));
+		
+		if (a < c) {
+			e.xrot = cotanQuantity;
+		} else {
+			e.xrot = halfPi + cotanQuantity;
+		}
+	}
+	
+	e.coeffs = [a, b, c, d, f, g];
+	
+	return e;
 };
